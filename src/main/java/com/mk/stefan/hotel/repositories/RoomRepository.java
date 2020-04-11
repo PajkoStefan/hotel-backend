4ddc4865e657fdde2e321c03ae12ce775d112330
PajkoStefan <52094853+PajkoStefan@users.noreply.github.com>
2020-04-11 20:52:29 +0200

    add basic RoomRepository and TableRepository controllers

---
+++ src/main/java/com/mk/stefan/hotel/repositories/RoomRepository.java
@@ -0,0 +1,7 @@
+package com.mk.stefan.hotel.repositories;
+
+import com.mk.stefan.hotel.model.Room;
+import org.springframework.data.repository.CrudRepository;
+
+public interface RoomRepository extends CrudRepository<Room, Long> {
+}
