88c6beab00c5b29846d501768e705bc8ff3b4e11
PajkoStefan <52094853+PajkoStefan@users.noreply.github.com>
2020-04-11 23:25:40 +0200

    add RoomReservation, TableReservation and Order in model

---
+++ src/main/java/com/mk/stefan/hotel/model/Order.java
@@ -0,0 +1,76 @@
+package com.mk.stefan.hotel.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity(name = "orders")
+@Getter
+@Setter
+@AllArgsConstructor
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "room_number")
+    private String roomNumber;
+
+    @Column(name = "bill_number")
+    private String billNumber;
+
+    @Column(name = "order_date")
+    private String startDate;
+
+    @Column(name = "order_time")
+    private String time;
+
+    public Order() {
+    }
+
+    public Order(String firstName, String lastName, String roomNumber, String billNumber, String startDate, String time) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.roomNumber = roomNumber;
+        this.billNumber = billNumber;
+        this.startDate = startDate;
+        this.time = time;
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", roomNumber='" + roomNumber + '\'' +
+                ", billNumber='" + billNumber + '\'' +
+                ", startDate='" + startDate + '\'' +
+                ", time='" + time + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Order order = (Order) o;
+
+        return id != null ? id.equals(order.id) : order.id == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+
+}
