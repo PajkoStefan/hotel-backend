b95fa3c56709cafa60605f48bb1d4d39fd2223ae
PajkoStefan <52094853+PajkoStefan@users.noreply.github.com>
2020-04-11 22:43:51 +0200

    add Service and Menu repositories

---
+++ src/main/java/com/mk/stefan/hotel/repositories/MenuRepository.java
@@ -0,0 +1,7 @@
+package com.mk.stefan.hotel.repositories;
+
+import com.mk.stefan.hotel.model.Menu;
+import org.springframework.data.repository.CrudRepository;
+
+public interface MenuRepository extends CrudRepository<Menu, Long> {
+}
---
+++ src/main/java/com/mk/stefan/hotel/repositories/ServiceRepository.java
@@ -0,0 +1,7 @@
+package com.mk.stefan.hotel.repositories;
+
+import com.mk.stefan.hotel.model.Service;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ServiceRepository extends CrudRepository<Service, Long> {
+}
