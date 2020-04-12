139dc5e559565bfc232a5fa9af9bae586179f62e
PajkoStefan <52094853+PajkoStefan@users.noreply.github.com>
2020-04-12 17:57:25 +0200

    add offer model

---
+++ src/main/java/com/mk/stefan/hotel/model/Offer.java
@@ -0,0 +1,61 @@
+package com.mk.stefan.hotel.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+public class Offer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String offer;
+
+    private String description;
+
+    private String price;
+
+    public Offer() {
+    }
+
+    public Offer(String offer, String description, String price) {
+        this.offer = offer;
+        this.description = description;
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Offer{" +
+                "offer='" + offer + '\'' +
+                ", description='" + description + '\'' +
+                ", price='" + price + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Offer offer = (Offer) o;
+
+        return id != null ? id.equals(offer.id) : offer.id == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+
+}
