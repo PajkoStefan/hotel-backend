4d0f745b2e282222b1260b042bad8b13daec102e
PajkoStefan <52094853+PajkoStefan@users.noreply.github.com>
2020-04-12 18:30:02 +0200

    add missing repositories to constructor in BootStrapData

--- src/main/java/com/mk/stefan/hotel/bootstrap/BootStrapData.java
+++ src/main/java/com/mk/stefan/hotel/bootstrap/BootStrapData.java
@@ -1,32 +1,51 @@
 package com.mk.stefan.hotel.bootstrap;
 
-import com.mk.stefan.hotel.model.Account;
-import com.mk.stefan.hotel.model.Login;
-import com.mk.stefan.hotel.model.SignUp;
-import com.mk.stefan.hotel.model.Room;
-import com.mk.stefan.hotel.model.Table;
 import com.mk.stefan.hotel.repositories.*;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 @Component
 public class BootStrapData implements CommandLineRunner {
+
     private final AccountRepository accountRepository;
     private final SignUpRepository signUpRepository;
     private final LoginRepository loginRepository;
     private final RoomRepository roomRepository;
     private final TableRepository tableRepository;
+    private final RoomReservationRepository roomReservationRepository;
+    private final TableReservationRepository tableReservationRepository;
+    private final OrderRepository orderRepository;
+    private final ServiceRepository serviceRepository;
+    private final MenuRepository menuRepository;
+    private final BillRepository billRepository;
+    private final PaymentRepository paymentRepository;
+    private final ContactUsRepository contactUsRepository;
+    private final OfferRepository offerRepository;
 
-    public BootStrapData(AccountRepository accountRepository,
-                         SignUpRepository signUpRepository,
-                         LoginRepository loginRepository,
-                         RoomRepository roomRepository,
-                         TableRepository tableRepository) {
+    public BootStrapData(AccountRepository accountRepository, SignUpRepository signUpRepository,
+                         LoginRepository loginRepository, RoomRepository roomRepository,
+                         TableRepository tableRepository,
+                         RoomReservationRepository roomReservationRepository,
+                         TableReservationRepository tableReservationRepository,
+                         OrderRepository orderRepository, ServiceRepository serviceRepository,
+                         MenuRepository menuRepository, BillRepository billRepository,
+                         PaymentRepository paymentRepository, ContactUsRepository contactUsRepository,
+                         OfferRepository offerRepository) {
+
         this.accountRepository = accountRepository;
         this.signUpRepository = signUpRepository;
         this.loginRepository = loginRepository;
         this.roomRepository = roomRepository;
         this.tableRepository = tableRepository;
+        this.roomReservationRepository = roomReservationRepository;
+        this.tableReservationRepository = tableReservationRepository;
+        this.orderRepository = orderRepository;
+        this.serviceRepository = serviceRepository;
+        this.menuRepository = menuRepository;
+        this.billRepository = billRepository;
+        this.paymentRepository = paymentRepository;
+        this.contactUsRepository = contactUsRepository;
+        this.offerRepository = offerRepository;
     }
 
     @Override
