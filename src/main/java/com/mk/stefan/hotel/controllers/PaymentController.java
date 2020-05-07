689d94e3615d8d75c59c1aae4718d984ce52a4dd
PajkoStefan <52094853+PajkoStefan@users.noreply.github.com>
2020-05-07 15:46:44 +0200

    add Payment service, api V1.0

--- src/main/java/com/mk/stefan/hotel/controllers/PaymentController.java
+++ src/main/java/com/mk/stefan/hotel/controllers/PaymentController.java
@@ -1,24 +1,49 @@
 package com.mk.stefan.hotel.controllers;
 
-import com.mk.stefan.hotel.repositories.PaymentRepository;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.mk.stefan.hotel.model.Payment;
+import com.mk.stefan.hotel.services.payment.PaymentService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
-@Controller
+import javax.servlet.http.HttpServletResponse;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin(origins = "http://localhost:3000")
 public class PaymentController {
 
-    private final PaymentRepository paymentRepository;
+    private final PaymentService paymentService;
 
-    public PaymentController(PaymentRepository paymentRepository) {
-        this.paymentRepository = paymentRepository;
+    public PaymentController(PaymentService paymentService) {
+        this.paymentService = paymentService;
     }
 
-    @RequestMapping("/payment")
-    public  String getPayments(Model model){
-
-        model.addAttribute("payments", paymentRepository.findAll());
-
-        return "paymentlist";
+    @GetMapping
+    @RequestMapping("/getallpayments")
+    public List<Payment> getAllPayments() {
+        return paymentService.getAllPayments();
     }
+
+    @PostMapping("/createpayment")
+    @ResponseStatus(HttpStatus.CREATED)
+    @ResponseBody
+    public Payment createNewPayment(@RequestBody Payment payment,
+                                    HttpServletResponse response,
+                                    UriComponentsBuilder builder){
+
+        Payment payment1 = new Payment(payment.getFullName(), payment.getCardNumber(),
+                payment.getSecurityCode(), payment.getExpirationDateMonth(),
+                payment.getExpirationDateYear());
+
+        response.setHeader("Location", builder.path("/api/createpayment/"+payment.getId()).
+                buildAndExpand(payment.getId()).toUriString());
+
+        return paymentService.createNewPayment(payment1.getFullName(),
+                payment1.getCardNumber(), payment1.getSecurityCode(),
+                payment1.getExpirationDateMonth(), payment1.getExpirationDateYear());
+    }
+
+
 }
--- src/main/java/com/mk/stefan/hotel/repositories/PaymentRepository.java
+++ src/main/java/com/mk/stefan/hotel/repositories/PaymentRepository.java
@@ -1,7 +1,8 @@
 package com.mk.stefan.hotel.repositories;
 
 import com.mk.stefan.hotel.model.Payment;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PaymentRepository extends CrudRepository<Payment, Long> {
+
+public interface PaymentRepository extends JpaRepository<Payment, Long> {
 }
