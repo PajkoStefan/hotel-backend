38b6af25701429cd43a3b7f3dfde40f974dae5f5
PajkoStefan <52094853+PajkoStefan@users.noreply.github.com>
2020-05-07 15:47:13 +0200

    add ContactUs service, api V1.0

--- src/main/java/com/mk/stefan/hotel/controllers/ContactUsController.java
+++ src/main/java/com/mk/stefan/hotel/controllers/ContactUsController.java
@@ -1,25 +1,44 @@
 package com.mk.stefan.hotel.controllers;
 
-import com.mk.stefan.hotel.repositories.ContactUsRepository;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.mk.stefan.hotel.model.ContactUs;
+import com.mk.stefan.hotel.services.contactus.ContactUsService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
-@Controller
+import javax.servlet.http.HttpServletResponse;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin(origins = "http://localhost:3000")
 public class ContactUsController {
 
-    private final ContactUsRepository contactUsRepository;
+    private final ContactUsService contactUsService;
 
-    public ContactUsController(ContactUsRepository contactUsRepository) {
-        this.contactUsRepository = contactUsRepository;
+    public ContactUsController(ContactUsService contactUsService) {
+        this.contactUsService = contactUsService;
     }
 
-    @RequestMapping("/contactus")
-    public  String getContactUsInfo(Model model){
+    @GetMapping("/getallcontactus")
+    public List<ContactUs> getAllInformationFromContactUs() {
+        return  contactUsService.getAllInformationFromContactUs();
+    }
 
-        model.addAttribute("contactusinfos", contactUsRepository.findAll());
-
-        return "contactusinfolist";
+    @PostMapping("/createcontactus")
+    @ResponseStatus(HttpStatus.CREATED)
+    @ResponseBody
+    public ContactUs createNewContactUs(@RequestBody ContactUs contactUs,
+                                        HttpServletResponse response,
+                                        UriComponentsBuilder builder){
+        ContactUs contactUs1 = new ContactUs(contactUs.getFirstName(),
+                contactUs.getLastName(), contactUs.getEmail(),
+                contactUs.getComment());
+        response.setHeader("Location", builder.path("/api/createcontactus/"+contactUs.getId()).
+                buildAndExpand(contactUs.getId()).toUriString());
+        return contactUsService.createNewContactUs(contactUs1.getFirstName(),
+                contactUs1.getLastName(), contactUs1.getEmail(),
+                contactUs1.getComment());
     }
 
 }
--- src/main/java/com/mk/stefan/hotel/repositories/ContactUsRepository.java
+++ src/main/java/com/mk/stefan/hotel/repositories/ContactUsRepository.java
@@ -1,7 +1,7 @@
 package com.mk.stefan.hotel.repositories;
 
 import com.mk.stefan.hotel.model.ContactUs;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ContactUsRepository extends CrudRepository<ContactUs, Long> {
+public interface ContactUsRepository extends JpaRepository<ContactUs, Long> {
 }
